83a81e0f5a8ef988d745c654dd4ec9f1626430e8
kongtouGit21 <kongtouGit21@outlook.com>
2023-07-27 23:29:13 +0800

    add Maths.lua

---
+++ Maths.lua
@@ -0,0 +1,20 @@
+io.write("5 + 3 = ", 5 + 3, "\n")
+io.write("5 - 3 = ", 5 - 3, "\n")
+io.write("5 * 3 = ", 5 * 3, "\n")
+io.write("5 / 3 = ", 5 / 3, "\n")
+io.write("5 % 3 = ", 5 % 3, "\n")
+io.write("7.2 % 3 = ", 6.2 % 3, "\n")
+
+io.write("floor(2.345) = ", math.floor(2.345), "\n")
+io.write("ceil(2.345) = ", math.ceil(2.345), "\n")
+io.write("max(2, 3) = ", math.max(2, 3), "\n")
+io.write("min(2, 3) = ", math.min(2, 3), "\n")
+io.write("pow(8, 2) = ", math.pow(8,2), "\n")
+io.write("sqrt(64) = ", math.sqrt(64), "\n")
+io.write("random() = ", math.random(), "\n")
+io.write("random(10) = ", math.random(10), "\n")
+io.write("random(10, 100) = ", math.random(10, 100), "\n")
+
+math.randomseed(os.time())
+
+print(string.format("Pi = %.10f", math.pi))
